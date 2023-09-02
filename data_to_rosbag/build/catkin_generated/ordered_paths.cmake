782e3d21700faa03896b89aa0089c659e08d0f34
zhuhang-linux <zhuhsgg@whu.edu.cn>
2023-09-02 11:37:50 +0800

    delete build

--- data_to_rosbag/build/catkin_generated/ordered_paths.cmake
+++
@@ -1,1 +0,0 @@
-set(ORDERED_PATHS "/media/zhuhang/F/gvins_ws/devel/lib;/opt/ros/noetic/lib")
--- data_to_rosbag/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /media/zhuhang/F/gvins_ws/src/data_to_rosbag/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
